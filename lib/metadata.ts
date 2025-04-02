6efc16f02d8d5e435ec0c77774b8904a931034ec
Ktora <130749910+ktoraco@users.noreply.github.com>
2025-04-02 09:19:54 +0000

    進捗、とりあえず表示はできた

---
+++ lib/metadata.ts
@@ -0,0 +1,6 @@
+// metadata.ts などのサーバーサイド用のファイルに移動
+export const metadata: { title: string; description: string } = {
+    title: "e-Snap Project",
+    description: "game世界のsnap写真を閲覧できるwebApp"
+  };
+  
