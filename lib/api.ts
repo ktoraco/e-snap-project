2894ee2d7ab5703d4c3eea44124e20c3f7a13323
Ktora <130749910+ktoraco@users.noreply.github.com>
2025-04-08 09:22:00 +0000

    microCMS接続しようとしてる途中

--- lib/api.ts
+++ lib/api.ts
@@ -1,12 +1,4 @@
 import axios from "axios";
-import { Game } from "./tapes";
+import { Game } from "./types";
 
-const API_URL = "https://your-microcms-endpoint";
-const API_KEY = "your-api-key";
-
-export const fetchGames = async (): Promise<Game[]> => {
-  const response = await axios.get(`${API_URL}/games`, {
-    headers: { "X-API-KEY": API_KEY },
-  });
-  return response.data.contens;
-};
+const API_URL = "https"
