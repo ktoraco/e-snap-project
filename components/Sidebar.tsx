d66b01af40e0c59350772fb3132cd76d51e6497e
kichitaro-p <kikukuphoto@gmail.com>
2025-04-10 22:59:03 +0900

    sidebar調整

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -50,19 +50,19 @@ const Sidebar: FC<SidebarProps> = ({ games, onGameClick }) => {
   };
 
   // モバイル用とデスクトップ用のサイズ調整
-  const sidebarWidth = isMobile ? "78px" : "100px";
+  const sidebarWidth = isMobile ? "w-[70px]" : "w-[90px]";
   const iconSize = isMobile ? "w-12 h-12" : "w-14 h-14";
-  const gameIconSize = isMobile ? "w-12" : "w-14";
+  const gameIconSize = isMobile ? "w-9 h-9" : "w-14 h-14";
 
   return (
-    <motion.div className="flex h-screen relative" initial={{ width: isOpen ? "0px" : sidebarWidth }} animate={{ width: isOpen ? "0px" : sidebarWidth }} transition={{ duration: 0.3 }}>
+    <motion.div className={`flex h-screen ${isOpen ? "w-0" : sidebarWidth} relative`} initial={{ width: isOpen ? 0 : isMobile ? 70 : 90 }} animate={{ width: isOpen ? 0 : isMobile ? 70 : 90 }} transition={{ duration: 0.3 }}>
       {/* サイドバーのトグルボタン - 位置を上部に調整 */}
       <button onClick={toggleSidebar} className={`absolute ${isMobile ? "bottom-24" : "bottom-28"} left-4 z-50 text-white rounded-md flex items-center justify-center ${iconSize}`}>
         <Image src={isOpen ? "/icons/sclose.svg" : "/icons/sopen.svg"} alt={isOpen ? "Close" : "Open"} width={48} height={48} className={`rounded-full ${iconSize}`} />
       </button>
 
       {/* サイドバーのコンテンツ */}
-      <div className={`transition-width duration-300 h-full bg-stone-900 justify-center text-white ${isOpen ? "w-0" : isMobile ? "w-12" : "w-14"}`}>
+      <div className={`transition-all duration-300 h-full bg-stone-900 flex flex-col items-center text-white ${isOpen ? "w-0" : "w-full"}`}>
         {/* ホームアイコン */}
         <motion.div className="flex flex-col pt-2 justify-center items-center p-top-2 mt-2 rounded-full cursor-pointer" onClick={() => router.push("/")} whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.1 }}>
           <CustomIcon isGradient={pathname === "/"} size={isMobile ? 30 : 36} />
